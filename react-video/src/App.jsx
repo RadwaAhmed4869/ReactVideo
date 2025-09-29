e78a4b2c28d05d76babddf1b5031024b46a56e4f
RadwaAhmed4869 <48102202+RadwaAhmed4869@users.noreply.github.com>
2025-09-29 15:58:13 +0300

    Update App.jsx

--- react-video/src/App.jsx
+++ react-video/src/App.jsx
@@ -1,26 +1,39 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import ReactPlayer from 'react-player'
 
 import './App.css'
 
 function App() {
+  const videoRef = useRef(null);
   const [play, setPlay] = useState(false)
 
   const handleReverse = () => {
-    
+    const fps = 30;
+    let intervalReverse = setInterval(() => {
+      if (videoRef.current.currentTime <= 0) {
+        clearInterval(intervalReverse);
+        videoRef.current.pause();
+      }
+      else {
+        videoRef.current.currentTime -= 1 / fps;
+      }
+    }, 1000 / fps);
   }
 
   const handlePlay = () => {
-    setPlay((prevState) => !prevState);
+    setPlay(true);
   }
 
 
   return (
     <>
       <ReactPlayer
+        ref={videoRef}
+        src='/assets/views.mp4'
+        width="100%"
         playing={play}
+        controls
         height={'100%'}
-        src='../public/assets/views.mp4' width="100%"
       />
       <button onClick={() => { handlePlay() }}>Play</button>
       <button onClick={() => { handleReverse() }}>Reverse</button>
@@ -29,3 +42,6 @@ function App() {
 }
 
 export default App
+
+
+// Resource: https://www.geeksforgeeks.org/html/how-to-play-video-in-reverse-in-html5/
